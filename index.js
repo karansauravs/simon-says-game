e411e99a44aa34e1c419f57a10cb6e384d3fee95
Karan Saurav <131188059+karansauravs@users.noreply.github.com>
2024-06-30 11:19:49 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,45 @@
+var buttonColors=["red","blue","green","yellow"];
+var gamePattern=[];
+var userClickedPattern = [];
+var started = false;
+var level = 0;
+
+$(document).keypress(function() {
+  if (!started) {
+    $("#level-title").text("Level " + level);
+    nextSequence();
+    started = true;
+  }
+});
+
+
+$(".btn").click(function() {
+  var userChosenColour = $(this).attr("id");
+  userClickedPattern.push(userChosenColour);
+  playSound(userChosenColour);
+  animatePress(userChosenColour);
+});
+
+
+function nextSequence(){
+    level++;
+     $("#level-title").text("Level " + level);
+    var randNum= Math.floor(Math.random()*4);
+    var randomChosenColour= buttonColors[randNum];
+    gamePattern.push(randomChosenColour);
+    console.log(randNum);
+    $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+    playSound(randomChosenColour);
+}
+
+function playSound(name){
+    var audio = new Audio("sounds/" + name + ".mp3");
+    audio.play();
+}
+
+function animatePress(currentColor){
+    $("#"+currentColor).addClass("pressed");
+    setTimeout(function(){
+        $("#"+currentColor).removeClass("pressed");
+    },100);
+}
